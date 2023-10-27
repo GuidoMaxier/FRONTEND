afb3a87eb7cdfeb3acd74d3df12a76b9c69de39e
rafaeliezer22 <montesbisco@gmail.com>
2023-10-26 23:06:40 -0300

    js

--- FAQ2/form/from2.js
+++ FAQ2/form/from2.js
@@ -1,3 +1,14 @@
+var inputs = document.getElementsByClassName('formulario__input');
+for (var i = 0; i < inputs.length; i++) {
+    inputs[i].addEventListener('keyup', function () {
+        if(this.value.length>=1) {
+            this.nextElementSibling.classList.add('fijar');
+        } else {
+            this.nextElementSibling.classList.remove('fijar');
+        }
+    });
+}
+
 document.addEventListener("DOMContentLoaded", function () {
     const form = document.getElementById("contacto-form");
     const nombres = document.getElementById("nombres");
