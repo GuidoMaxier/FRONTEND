95fdf52128f21799a75f0b2254f97a7aa981f55e
Hernan Casasola <67485423+GuidoMaxier@users.noreply.github.com>
2023-10-27 02:22:26 -0300

    login /sidebar
    - se le dio cuncionalidad a cerrar session
    - ocultamos "iniciar session" cuando el usuario ya esta logeado.
    - se crearon 4 cuenta de admin, con distintos perfiles (misma clave)

--- sidebar.js
+++ sidebar.js
@@ -1,4 +1,3 @@
-/* Created by Tivotal */
 
 let btn = document.querySelector(".fa-dice-d20");
 let sidebar = document.querySelector(".sidebar");
@@ -8,7 +7,7 @@ btn.addEventListener("click", () => {
 });
 
 let arrows = document.querySelectorAll(".arrow");
-for (var i = 0; i < arrows.length; i++) {
+for (let i = 0; i < arrows.length; i++) {
   arrows[i].addEventListener("click", (e) => {
     let arrowParent = e.target.parentElement.parentElement;
 
@@ -18,23 +17,23 @@ for (var i = 0; i < arrows.length; i++) {
 
 
 
-  document.addEventListener("DOMContentLoaded", function () {
-    const menuLinks = document.querySelectorAll(".nav-list a");
+  // document.addEventListener("DOMContentLoaded", function () {
+  //   const menuLinks = document.querySelectorAll(".nav-list a");
 
-    menuLinks.forEach((link) => {
-      link.addEventListener("click", function (event) {
-        event.preventDefault(); // Evita el comportamiento predeterminado de los enlaces.
+  //   menuLinks.forEach((link) => {
+  //     link.addEventListener("click", function (event) {
+        // event.preventDefault(); // Evita el comportamiento predeterminado de los enlaces.
 
-        const dataSrc = link.getAttribute("href"); // Obtiene la URL de destino.
-        const iframe = document.getElementById("content-frame");
+        // const dataSrc = link.getAttribute("href"); // Obtiene la URL de destino.
+        // const iframe = document.getElementById("content-frame");
 
-        if (dataSrc && iframe) {
-            iframe.src = dataSrc; // Carga la URL de destino en el iframe.
+        // if (dataSrc && iframe) {
+            // iframe.src = dataSrc; // Carga la URL de destino en el iframe.
           
-        }
-      });
-    });
-  });
+  //       }
+  //     });
+  //   });
+  // });
 
 
 
@@ -42,8 +41,9 @@ for (var i = 0; i < arrows.length; i++) {
 window.addEventListener("message", function (event) {
   // Verifica si el mensaje proviene del iframe y si contiene la información del usuario
   if (event.source === iframeElement.contentWindow && event.data === "usuarioLogeado") {
-      // Realiza la actualización de la página contenedora aquí
-      location.reload(); // O cualquier otra acción que necesites
+      // Actualizamos el contenedor
+      location.reload(); 
+
   }
 });
 
@@ -51,4 +51,58 @@ window.addEventListener("message", function (event) {
 const iframeElement = document.getElementById("content-frame");
 
 
+
+document.addEventListener("DOMContentLoaded", function () {
+  const menuLinks = document.querySelectorAll(".nav-list a");
+  const iframeElement = document.getElementById("content-frame");
+  const usuarioLogeado = sessionStorage.getItem("usuarioLogeado");
+
+  menuLinks.forEach((link) => {
+    link.addEventListener("click", function (event) {
+      event.preventDefault();
+      const dataSrc = link.getAttribute("href");
+      if (dataSrc && iframeElement) {
+        iframeElement.src = dataSrc;
+      }
+    });
+  });
+
+  if (usuarioLogeado) {
+
+    const loginMenu = document.querySelector(".login-menu");
+    if (loginMenu) {
+      loginMenu.style.display = "none";
+    }
+
+    const usuarioData = JSON.parse(usuarioLogeado);
+    const profileImage = document.querySelector(".profile-content img");
+    profileImage.src = usuarioData.avatar;
+
+    const profileName = document.querySelector(".name-job .name");
+    profileName.textContent = usuarioData.nombre;
+
+    const profileJob = document.querySelector(".name-job .job");
+    profileJob.textContent = usuarioData.rol;
+
+    const profileMenu = document.querySelector(".profile-menu");
+    const logoutMenu = document.querySelector(".logout-menu");
+    const profileDetails = document.querySelector(".profile-details");
+
+    if (logoutMenu && profileDetails) {
+      logoutMenu.style.display = "flex";
+      profileDetails.style.display = "flex";
+      profileMenu.style.display = "flex";
+    }
+    // Agregar un evento de clic al botón de cierre de sesión
+    const logoutButton = document.getElementById("logout-button");
+    if (logoutButton) {
+      logoutButton.addEventListener("click", function () {
+        // Eliminar la información de la sesión
+        sessionStorage.removeItem("usuarioLogeado");
+        // Recargar la página
+        location.reload();
+      });
+    }
+  }
+});
 
